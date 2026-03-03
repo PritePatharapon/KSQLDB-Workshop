9bdccc199025bfe4c02177259c07e9c4c177d48e
worapat_mfec <worapat@mfec.co.th>
2026-03-03 18:05:58 +0700

    version final

--- original_script/02_Pipeline/02_stream_join_table/01_create.sql
+++ original_script/02_Pipeline/02_stream_join_table/01_create.sql
@@ -1,5 +1,5 @@
 ---Step 1 Create source Table
-CREATE TABLE "CDC-DB-MASTER-ACC-RAW-TB-<USER>" (
+CREATE TABLE "CDC_DB_MASTER_ACC_RAW_TB_<USER>" (
   ACCOUNT_ID VARCHAR PRIMARY KEY,
   ACCOUNT_NAME VARCHAR,
   ACCOUNT_BALANCE DOUBLE,
@@ -7,15 +7,15 @@ CREATE TABLE "CDC-DB-MASTER-ACC-RAW-TB-<USER>" (
   UPDATE_TS TIMESTAMP,
   __OP STRING
 ) WITH (
-  KAFKA_TOPIC = 'CDC-DB-MASTER-ACC-RAW-TB-<USER>',  -- Source Kafka topic
+  KAFKA_TOPIC = 'CDC_DB_MASTER_ACC_RAW_TB_<USER>',  -- Source Kafka topic
   FORMAT = 'JSON',               -- JSON message format
   PARTITIONS = 3,                -- Number of partitions for scalability
   REPLICAS = 1                   -- Replication factor for fault tolerance
 );
 
 ---Step 2 Enrichment Stream with Table
-CREATE STREAM "CDC-DB-MASTER-ACC-STG-JOIN-STREAM-TABLE-ST-<USER>" WITH (
-  KAFKA_TOPIC = 'CDC-DB-MASTER-ACC-STG-JOIN-STREAM-TABLE-ST-<USER>',      -- Source Kafka topic
+CREATE STREAM "CDC_DB_MASTER_ACC_STG_JOIN_STREAM_TABLE_ST_<USER>" WITH (
+  KAFKA_TOPIC = 'CDC_DB_MASTER_ACC_STG_JOIN_STREAM_TABLE_ST_<USER>',      -- Source Kafka topic
   FORMAT = 'JSON',               -- JSON message format
   PARTITIONS = 3,                -- Number of partitions for scalability
   REPLICAS = 1                   -- Replication factor for fault tolerance
@@ -28,6 +28,6 @@ SELECT
     A.ACCOUNT_NAME AS ACCOUNT_NAME,
     A.ACCOUNT_TYPE AS ACCOUNT_TYPE,
     A.ACCOUNT_BALANCE AS ACCOUNT_BALANCE
-FROM "CDC-MF-TXN-STG-ST-<USER>" T
-LEFT JOIN "CDC-DB-MASTER-ACC-RAW-TB-<USER>" A    
+FROM "CDC_MF_TXN_STG_ST_<USER>" T
+LEFT JOIN "CDC_DB_MASTER_ACC_RAW_TB_<USER>" A    
 ON T.ACC_NO = A.ACCOUNT_ID;
